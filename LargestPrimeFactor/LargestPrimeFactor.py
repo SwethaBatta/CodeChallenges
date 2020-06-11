8b64dcb6c89948912ed7bfafa26ae7b2c8195338
SwethaBatta <33192347+SwethaBatta@users.noreply.github.com>
2020-06-11 17:54:43 -0400

    Add files via upload

---
+++ LargestPrimeFactor/LargestPrimeFactor.py
@@ -0,0 +1,31 @@
+import math
+import sys
+
+
+def validNumber(num : int):
+    return num > 0 and num < sys.maxsize and num > -sys.maxsize and isinstance(num, int)
+    
+def largestPrimeFactor(num):
+    maxPrimeFactor = -1
+    inputNum = num
+    
+    if not validNumber(num):
+        return (f"Largest prime factor of {inputNum} : Not a valid value. Try again!")
+    
+    #If number is even, 2 is the maximum prime factor
+    while num%2 == 0:
+        maxPrimeFactor = 2
+        num /= 2
+        
+    #If not even, this odd loop executes to find the maximum prime factor
+    for i in range(3, int(math.sqrt(num))+1, 2):
+        while num % i == 0:
+            maxPrimeFactor = i
+            num /= i
+    
+    #If prime number greater than 2
+    if num > 2:
+        maxPrimeFactor = num
+    
+    return (f'Largest prime factor of {inputNum} :  {int(maxPrimeFactor)}')
+
